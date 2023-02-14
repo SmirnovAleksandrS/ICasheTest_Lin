7468cdf83c3e1f4133fc417b585bfbe71d6e9723
Sergei Miroshnichenko <s.miroshnichenko@yadro.com>
2023-02-14 12:04:44 +0300

    qemu script: Can run with -S to stop at the start, waiting for gdb

--- Edu-Barriers/rv-optimizations-demo-1/8_run.sh
+++ Edu-Barriers/rv-optimizations-demo-1/8_run.sh
@@ -13,4 +13,5 @@
     -append "console=ttyS0 earlycon root=/dev/ram0 rw rdinit=/init" \
     -device virtio-net-device,netdev=net \
     -netdev user,id=net,hostfwd=tcp::2345-:2345,hostfwd=tcp::10022-:22 \
-    -gdb tcp::1234
+    -gdb tcp::1234 \
+    $1
