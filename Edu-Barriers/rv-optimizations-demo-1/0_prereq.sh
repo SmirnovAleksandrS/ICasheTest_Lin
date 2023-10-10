b408993259adbcf2def03e4758ee38f6ea3ff5e1
SergMir <dartzerg@gmail.com>
2023-10-10 12:48:11 +0300

    buildroot: Bump to 2023.08.1

--- Edu-Barriers/rv-optimizations-demo-1/0_prereq.sh
+++ Edu-Barriers/rv-optimizations-demo-1/0_prereq.sh
@@ -5,11 +5,11 @@
 [ -z "`grep safe-path ~/.gdbinit`" ] && echo 'Add "set auto-load safe-path /" to your ~/.gdbinit (or limit to this directory)'
 
 # Fedora
-#sudo dnf install ncurses-devel perl-ExtUtils-MakeMaker ninja-build glib2-devel pixman-devel libslirp-devel
+#sudo dnf install ncurses-devel perl-ExtUtils-MakeMaker ninja-build glib2-devel pixman-devel libslirp-devel perl-IPC-Cmd
 # Ubuntu
 #sudo apt install libncurses-dev libextutils-makemaker-cpanfile-perl ninja-build libglib2.0-dev libpixman-1-dev libslirp-dev
 
-git clone -b 2023.05.1 git://git.busybox.net/buildroot
+git clone -b 2023.08.1 git://git.busybox.net/buildroot
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 cd linux; git checkout v6.4; cd ..
 git clone https://github.com/riscv-software-src/opensbi.git
