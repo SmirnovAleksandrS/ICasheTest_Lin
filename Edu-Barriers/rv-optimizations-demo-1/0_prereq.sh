558d90116034e441271e356e74011d87f784adaa
SergMir <dartzerg@gmail.com>
2024-02-27 23:11:01 +0300

    buildroot: Bump to 2023.11.1

--- Edu-Barriers/rv-optimizations-demo-1/0_prereq.sh
+++ Edu-Barriers/rv-optimizations-demo-1/0_prereq.sh
@@ -9,7 +9,7 @@
 # Ubuntu
 #sudo apt install libncurses-dev libextutils-makemaker-cpanfile-perl ninja-build libglib2.0-dev libpixman-1-dev libslirp-dev
 
-git clone -b 2023.08.1 git://git.busybox.net/buildroot
+git clone -b 2023.11.1 git://git.busybox.net/buildroot
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 cd linux; git checkout v6.5; cd ..
 git clone https://github.com/riscv-software-src/opensbi.git
