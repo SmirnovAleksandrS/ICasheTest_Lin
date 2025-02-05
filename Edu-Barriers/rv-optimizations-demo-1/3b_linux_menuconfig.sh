996a5a9778d81cecbfd4ebec9afae6302e23258d
Sergei Miroshnichenko <s.miroshnichenko@yadro.com>
2025-02-05 13:30:45 +0300

    linux kernel: Bump to v6.13

--- Edu-Barriers/rv-optimizations-demo-1/3b_linux_menuconfig.sh
+++ Edu-Barriers/rv-optimizations-demo-1/3b_linux_menuconfig.sh
@@ -3,4 +3,5 @@
 . ./config.sh
 
 cd linux && make ARCH=riscv CROSS_COMPILE=$CROSS_COMPILE menuconfig; cd ..
+#cd linux && make ARCH=riscv CROSS_COMPILE=$CROSS_COMPILE oldconfig; cd ..
 cp linux/.config linux_config
